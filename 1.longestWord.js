723bfeed4b0802bf967d4e1318f3a0d819853399
sachin <sachin.sriva8@gmail.com>
2023-07-20 00:44:28 +0530

    modified code

--- 1.longestWord.js
+++ 1.longestWord.js
@@ -2,26 +2,22 @@
  * 
  * @param {string} str
  * @step1 create a function to take input from user (str)
- * @step2 break string into array of words. Declare and initialize max_len to zero.
- * @step3 calculating max_length of longest word by iterating though the array.
- * @step4 now pushing the word whose length matches to above calculated max_length into new array out.
- * @step5 return the first elemnt of out array.
+ * @step2 break string into array of words. create a variable temp with empty string.
+ * @step3 run a for loop from i=0 till i<arr.length-1 and check
+ *        if length of element at i is greater than or equal to elemnt at i+1. if yes then swap their values.
+ * @step4 return last elemnt of arr.
  */
 let longestWord=(str)=>{
     let arr= str.split(" ");
-    let out=[];
-    let max_len=0;
-    for(let i =0; i<arr.length;i++){
-        if (max_len<arr[i].length){
-            max_len=arr[i].length;
+    let temp='';
+    for(let i =0; i<arr.length-1;i++){
+        if (arr[i].length>=arr[i+1].length){
+            temp=arr[i+1];
+            arr[i+1]=arr[i];
+            arr[i]=temp;
         }
     }
-    for (let i of arr){
-        if(max_len==i.length){
-            out.push(i);
-        }
-    }
-    return out[0];
+    return arr[arr.length-1];
 }
 
 console.log(longestWord("My name is ankit jain & i'm from morena morema"));
