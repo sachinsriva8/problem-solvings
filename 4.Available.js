5c8c9604473a7e438be9cc6ea551a214f5d2b5db
sachin <sachin.sriva8@gmail.com>
2023-08-16 15:00:34 +0530

    Using Reduce

--- 4.Available.js
+++ 4.Available.js
@@ -29,15 +29,30 @@ const p = [
     },
   ];
 
-//   const out=
   const Available=p.map(({...e})=>{
     if(e.isActive){
         e.isActive="Available";
     }else{
-        e.isActive="NotAvailable";
+        e.isActive="Not Available";
     }
     return e;
   })
 
   // console.log("ori",p);
   console.log("updated",Available);
+
+  const availableUsingReduce=p.reduce((acc,{...cur})=>{
+    // console.log(cur);
+    if(cur.isActive){
+      cur.isActive="Available";
+    }else{
+      cur.isActive="Not Available";
+    }
+    acc.push(cur);
+    return acc;
+  },[]);
+
+  console.log("UpdatedUsingReduce",availableUsingReduce);
+
+
+
